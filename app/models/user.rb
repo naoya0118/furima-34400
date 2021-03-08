178d16a399924d115f5833b0169c39cb69feca87
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-08 15:48:10 +0900

    バリデーション　定数表記変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  full_name = /\A[ぁ-んァ-ン一-龥々]+\z/
-  kana_name = /\A[ァ-ヶー－]+\z/
+  FULL_NAME = /\A[ぁ-んァ-ン一-龥々]+\z/
+  KANA_NAME = /\A[ァ-ヶー－]+\z/
 
   with_options presence: true do
     validates :nickname 
     validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: 'には英字と数字の両方を含めて設定してください' }
-    validates :last_name, format: { with: full_name, message: '漢字、カタカナ、平仮名で入力してください' }
-    validates :first_name,format: { with: full_name, message: '漢字、カタカナ、平仮名で入力してください' }
-    validates :kana_last_name,format: { with: kana_name, message: 'カタカナで入力してください' }
-    validates :kana_first_name,format: { with: kana_name, message: 'カタカナで入力してください' }
+    validates :last_name, format: { with: FULL_NAME, message: '漢字、カタカナ、平仮名で入力してください' }
+    validates :first_name,format: { with: FULL_NAME, message: '漢字、カタカナ、平仮名で入力してください' }
+    validates :kana_last_name,format: { with: KANA_NAME, message: 'カタカナで入力してください' }
+    validates :kana_first_name,format: { with: KANA_NAME, message: 'カタカナで入力してください' }
     validates :birthday
   end
 
