c2063261fa2a47cba4a190f63fa5f562de28fb29
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-08 19:33:24 +0900

    ActiveStorage導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    
+  end
+
+  private
+  def message_params
+    params.require(:items).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,11 @@
 class Item < ApplicationRecord
 
-  #validates :title, presence: true
-  #validates :explanation, presence:true
-  #validates :price, presence: true
-  #validates :image, presence: true
+  validates :title, presence: true
+  validates :explanation, presence:true
+  validates :price, presence: true
+  validates :image, presence: true
   
-  #- belongs_to :user
+  belongs_to :user
+  has_one_attached :image
   #- has_one :buy
 end
