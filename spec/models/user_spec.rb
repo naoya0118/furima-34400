2e88a0b8724dab5a13391826dda581220b9f4571
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-08 16:21:40 +0900

    ログイン、パス修正　テストコード追加（emailに＠））

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,6 +29,11 @@ RSpec.describe User, type: :model do
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
+      it 'emailに@が含まれていない場合登録できない' do
+        @user.email = 'hog.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
       it "passwordが空では登録できない" do
         @user.password = ''
         @user.valid?
