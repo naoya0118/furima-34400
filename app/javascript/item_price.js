9a84b53e70a8021897a6e9f1cf2d298bb801c335
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-10 16:22:03 +0900

    販売利益の実装

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,5 +4,7 @@ priceInput.addEventListener("input", () => {
   const inputValue = priceInput.value;
   const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue / 10)
+  const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = Math.floor(inputValue * 0.9)
   })
 });
