716592dee7cd95e710508bc8ff5684e3f32f1e52
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-11 18:43:01 +0900

    実装完了

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :title, :explanation, :price, :category_id, :state_id, :delivery_fee_id, :delivery_area_id, :delivery_date_id).merge(user_id: current_user.id)
