054717b10953cb2a058eb9fa402f5c09879e744c
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-11 15:02:16 +0900

    詳細ページの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :title, :explanation, :price, :category_id, :state_id, :delivery_fee_id, :delivery_area_id, :delivery_date_id).merge(user_id: current_user.id)
