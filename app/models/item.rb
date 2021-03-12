aabd7c376ee0b6dafba90296a217023524d19bfc
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-12 12:56:42 +0900

    位置の変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :delivery_fee
+  belongs_to :delivery_area
+  belongs_to :delivery_date
+  belongs_to :user
+  has_one_attached :image
+  #- has_one :buy
 
   with_options presence: true do
     validates :title
@@ -20,12 +28,4 @@ class Item < ApplicationRecord
     validates :state_id
   end
 
-  belongs_to :category
-  belongs_to :state
-  belongs_to :delivery_fee
-  belongs_to :delivery_area
-  belongs_to :delivery_date
-  belongs_to :user
-  has_one_attached :image
-  #- has_one :buy
 end
