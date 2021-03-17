241ac612d4ffcc14e317bdbfba7ac18db01fe105
naoya0118 <yn.emerald.brand@gmail.com>
2021-03-17 16:32:07 +0900

    トークン生成の記述、復活

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -14,6 +14,9 @@ const pay = () => {
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
       document.getElementById("card-number").removeAttribute("name");
       document.getElementById("card-cvc").removeAttribute("name");
